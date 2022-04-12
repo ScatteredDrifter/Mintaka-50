20ce79a6aac45f0a62ab4e732a52b6a60df67d24
ScatteredDrifter <fabiansta@protonmail.com>
2022-04-12 14:13:55 +0200

    created, fixed firmware

---
+++ firmware/mintaka50/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#pragma once 
+#define VIAL_KEYBOARD_UID {0x67, 0x07, 0x1D, 0xF1, 0x69, 0x8A, 0x83, 0x90}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0}
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1}
+
+
